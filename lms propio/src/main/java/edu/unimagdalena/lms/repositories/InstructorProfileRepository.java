14a37f8910410a296f9d75e0503cce411afbb460
jjpalomino09 <jjpalomino@unimagdalena.edu.co>
2026-03-16 18:31:10 -0500

    Delete lms propio directory

--- lms propio/src/main/java/edu/unimagdalena/lms/repositories/InstructorProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.unimagdalena.lms.repositories;
-
-import edu.unimagdalena.lms.entities.InstructorProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructorProfileRepository extends JpaRepository<InstructorProfile, Long> {
-
-}
