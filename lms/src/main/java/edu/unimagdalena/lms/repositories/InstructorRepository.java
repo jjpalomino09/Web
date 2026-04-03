272408892f98ca0d7a5e0240c77666bfbdb80775
Victor Diaz <144868821+Victor-Diaz-18@users.noreply.github.com>
2026-04-03 12:08:01 -0500

    Delete lms directory

--- lms/src/main/java/edu/unimagdalena/lms/repositories/InstructorRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.unimagdalena.lms.repositories;
-
-import edu.unimagdalena.lms.entities.Instructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface InstructorRepository extends JpaRepository<Instructor, Long> {
-    Optional<Instructor> findByEmail(String email);
-}
--- lms/src/main/java/edu/unimagdalena/lms/repositories/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.unimagdalena.lms.repositories;
-
-import edu.unimagdalena.lms.entities.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    Optional<Student> findByEmail(String email);
-}
